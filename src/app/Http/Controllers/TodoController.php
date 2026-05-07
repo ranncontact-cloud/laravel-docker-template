ff23aae43b41583ac28b3e3820fda994ea1fe4ef
Taisei <Rann.contact@gmail.com>
2026-05-07 12:52:50 +0900

    TodoController内に新規viwe関数を作成し[ToDoを追加]ボタン押下時新規作成画面へ遷移するよう引数をcreate.blade.indexとした

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -6,12 +6,18 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+
+    public function create()
+    {
+        return view('todo.create');
+    }
+
     public function index()
     {
         // 追加
         $todo = new Todo();
         $todos = $todo->all();
-        
+
         return view('todo.index', ['todos' => $todos]);
     }
 }
