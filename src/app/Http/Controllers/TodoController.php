43e77d92f8b3f7680417cc1999705c4e74255331
Taisei <Rann.contact@gmail.com>
2026-05-07 11:38:12 +0900

    Model実装によりtodosテーブルとの紐付けを実行

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        // 追加
+        $todo = new Todo();
+        $todos = $todo->all();
         return view('todo.index');
     }
 }
+
+// use Illuminate\Http\Request;
+
+// class TodoController extends Controller
+// {
+//     public function index()
+//     {
+//         return view('todo.index');
+//     }
+// }
