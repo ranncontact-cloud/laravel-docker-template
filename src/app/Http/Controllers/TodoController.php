ba9a5e701d4ea9d061b840f5989c92b627945381
Taisei <Rann.contact@gmail.com>
2026-05-08 15:04:23 +0900

    詳細機能表示の実装。show.bladeの作成とそれに繋げるためのルートパラメータやshowメソッドを作成

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -39,6 +39,14 @@ class TodoController extends Controller
 
     return redirect()->route('todo.index');
     }
+
+    public function show($id)
+    {
+        $model = new Todo();
+        $todo = $model->find($id);
+
+        return view('todo.show', ['todo' => $todo]);
+    }
 }
 
 // use Illuminate\Http\Request;
